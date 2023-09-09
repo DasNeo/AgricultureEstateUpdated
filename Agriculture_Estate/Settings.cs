d1b2b2a4b51e0ae6c07d6120c62719620e49a962
S-Katagiri <katagiri.stsh@gmail.com>
2023-09-09 23:32:17 +0900

    MCM localization

--- Agriculture_Estate/Settings.cs
+++ Agriculture_Estate/Settings.cs
@@ -12,23 +12,23 @@ namespace AgricultureEstate
     public class Settings : AttributeGlobalSettings<Settings>
     {
 
-        [SettingPropertyInteger("Developed Plot Buy Price", 0, 100000, RequireRestart = false)]
+        [SettingPropertyInteger("{=agricultureestate_mcm_dev_buy}Developed Plot Buy Price", 0, 100000, RequireRestart = false)]
         public int PlotBuyPrice { get; set; } = 800;
-        [SettingPropertyInteger("Developed Plot Sell Price", 0, 100000, RequireRestart = false)]
+        [SettingPropertyInteger("{=agricultureestate_mcm_dev_sell}Developed Plot Sell Price", 0, 100000, RequireRestart = false)]
         public int PlotSellPrice { get; set; } = 200;
-        [SettingPropertyInteger("Undeveloped Plot Buy Price", 0, 100000, RequireRestart = false)]
+        [SettingPropertyInteger("{==agricultureestate_mcm_undev_buy}Undeveloped Plot Buy Price", 0, 100000, RequireRestart = false)]
         public int UndevelopedPlotBuyPrice { get; set; } = 400;
-        [SettingPropertyInteger("Undeveloped Plot Sell Price", 0, 100000, RequireRestart = false)]
+        [SettingPropertyInteger("{==agricultureestate_mcm_undev_sell}Undeveloped Plot Sell Price", 0, 100000, RequireRestart = false)]
         public int UndevelopedPlotSellPrice { get; set; } = 200;
-        [SettingPropertyInteger("Project Cost", 0, 500000, RequireRestart = false)]
+        [SettingPropertyInteger("{=agricultureestate_mcm_proj_cost}Project Cost", 0, 500000, RequireRestart = false)]
         public int ProjectCost { get; set; } = 20000;
-        [SettingPropertyInteger("Project Time", 1, 100, RequireRestart = false)]
+        [SettingPropertyInteger("{=agricultureestate_mcm_proj_time}Project Time", 1, 100, RequireRestart = false)]
         public int ProjectTime { get; set; } = 10;
-        [SettingPropertyFloatingInteger("Scaling of rent for plots", 0, 20, RequireRestart = false)]
+        [SettingPropertyFloatingInteger("{=agricultureestate_mcm_scale_rent}Scaling of rent for plots", 0, 20, RequireRestart = false)]
         public float LandRentScale { get; set; } = 1f;
-        [SettingPropertyFloatingInteger("Scaling of slave production", 0, 20, RequireRestart = false)]
+        [SettingPropertyFloatingInteger("{=agricultureestate_mcm_scale_production}Scaling of slave production", 0, 20, RequireRestart = false)]
         public float SlaveProductionScale { get; set; } = 1f;
-        [SettingPropertyFloatingInteger("Slave decline rate modifier", 0, 2, RequireRestart = false, HintText = "Setting the modifier to 0 disables slave decline.")]
+        [SettingPropertyFloatingInteger("{=agricultureestate_mcm_slave_decline}Slave decline rate modifier", 0, 2, RequireRestart = false, HintText = "Setting the modifier to 0 disables slave decline.")]
         public float SlaveDeclineModifier { get; set; } = 1f;
 
         public override string Id => "AgricultureEstate";
